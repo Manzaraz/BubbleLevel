a41bb71a5aba221a13fa55a121390e70bb629634
Christian Manzaraz <magnusmanz@gmail.com>
2024-05-18 19:34:10 -0300

    Section 2 Orientation Data View Learn to display motion data as string values in the view. DoubleExtension

---
+++ BubbleLevel/DoubleExtension.swift
@@ -0,0 +1,20 @@
+//
+//  DoubleExtension.swift
+//  BubbleLevel
+//
+//  Created by Christian Manzaraz on 18/05/2024.
+//
+
+import Foundation
+
+extension Double {
+    func describeAsFixedLengthString(integerDigits: Int = 2, fractionDigits: Int = 2) -> String {
+        self.formatted(
+            .number
+                .sign(strategy: .always())
+                .precision(
+                    .integerAndFractionLength(integer: integerDigits, fraction: fractionDigits)
+                )
+        )
+    }
+}
---
+++ BubbleLevel/OrientationDataView.swift
@@ -0,0 +1,40 @@
+//
+//  OrientationDataView.swift
+//  BubbleLevel
+//
+//  Created by Christian Manzaraz on 18/05/2024.
+//
+
+import SwiftUI
+
+struct OrientationDataView: View {
+    @EnvironmentObject var detector: MotionDetector
+    
+    var rollString: String {
+        detector.roll.describeAsFixedLengthString()
+    }
+    
+    var pitchString: String {
+        detector.pitch.describeAsFixedLengthString()
+    }
+    
+    var body: some View {
+        VStack {
+            Text("Horizontal: " + rollString)
+                .font(.system(.body, design: .monospaced))
+            Text("Vertical: " + pitchString)
+                .font(.system(.body, design: .monospaced))
+        }
+    }
+}
+
+struct OrientationDataView_Previews: PreviewProvider {
+    @StateObject static private var motionDetector = MotionDetector(updateInterval: 0.01).started()
+    
+    static var previews: some View {
+        OrientationDataView()
+            .environmentObject(motionDetector)
+    
+    }
+}
+
