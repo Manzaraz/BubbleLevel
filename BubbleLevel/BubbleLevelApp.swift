00080e71af61bde3f92a7741a3946f8dd5d2bcd3
Christian Manzaraz <magnusmanz@gmail.com>
2024-05-18 17:48:36 -0300

    Initial Commit

---
+++ BubbleLevel/BubbleLevelApp.swift
@@ -0,0 +1,17 @@
+//
+//  BubbleLevelApp.swift
+//  BubbleLevel
+//
+//  Created by Christian Manzaraz on 18/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct BubbleLevelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
