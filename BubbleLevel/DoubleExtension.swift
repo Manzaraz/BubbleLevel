dd7437362e286ac4cf97c6ba60a2b1407a2fd4b8
Christian Manzaraz <magnusmanz@gmail.com>
2024-05-18 22:01:10 -0300

    Section 3 Bubble Level View Learn to display the tilt of your device using a bubble level UI. Section 4 Double Extension Investigate using an extension on Double to format them in an easy to read way.

--- BubbleLevel/DoubleExtension.swift
+++ BubbleLevel/DoubleExtension.swift
@@ -8,12 +8,12 @@
 import Foundation
 
 extension Double {
-    func describeAsFixedLengthString(integerDigits: Int = 2, fractionDigits: Int = 2) -> String {
+    func describeAsFixedLengthString(integerDigits: Int = 2, frationDigits: Int = 2) -> String {
         self.formatted(
             .number
                 .sign(strategy: .always())
                 .precision(
-                    .integerAndFractionLength(integer: integerDigits, fraction: fractionDigits)
+                    .integerAndFractionLength(integer: integerDigits, fraction: frationDigits)
                 )
         )
     }
